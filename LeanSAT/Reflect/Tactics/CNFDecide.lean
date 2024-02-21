e78436af328ea92285ea65226863838acae1e230
Henrik Böving <hargonix@gmail.com>
2024-02-21 13:16:29 +0100

    feat: Reflect knows about custom SAT encodings

--- LeanSAT/Reflect/Tactics/CNFDecide.lean
+++ LeanSAT/Reflect/Tactics/CNFDecide.lean
@@ -12,19 +12,24 @@ open Lean Meta ReflectSat
 /--
 Interface for an external SAT solver with a verified certificate checker.
 
+The type `α` representing the specific encoding of `CNF Nat` that the `Solver`
+backend uses.
+
 The type `β` representing the unsatisfiability certificate should have `ToExpr β`,
 because we will need to embed the certificate in the generated proof.
 -/
-structure Solver (β : Type) where
+structure Solver (α : Type) (β : Type) where
+  /-- Encode the generic `CNF Nat` into the `Solver` specific CNF format. -/
+  encodeCNF : CNF Nat → α
   /-- An IO function that retrieves an opaque certificate of type `β`. -/
-  runExternal : CNF Nat → IO β
+  runExternal : α → IO β
   /--
   A verification function,
-  where `verify c b = true` if `b` is a valid certificate of the unsatisfiability of `c`.
+  where `verify (encodeCNF c) b = true` if `b` is a valid certificate of the unsatisfiability of `c`.
   -/
-  verify : CNF Nat → β → Bool
+  verify : α → β → Bool
   /-- Proof of the correctness of the verification function. -/
-  correct : ∀ c b, verify c b = true → c.unsat
+  correct : ∀ c b, verify (encodeCNF c) b = true → c.unsat
 
 /--
 We can lift a `Solver β` to a function `CNF Nat → MetaM Expr`,
@@ -32,20 +37,23 @@ which given `x : CNF Nat` produces a proof of `x.unsat`.
 
 But we need to jump through some hoops!
 -/
-def Solver.lift (solverName : Name) (certType : Expr) [ToExpr β] (s : Solver β) (c : CNF Nat) :
+def Solver.lift (solverName : Name) (cnfType : Expr) (certType : Expr) [ToExpr β] (s : Solver α β) (c : CNF Nat) :
     MetaM Expr := do
-  let b ← s.runExternal c
-  return mkApp5 (.const ``Solver.correct []) certType
+  let encoded := s.encodeCNF c
+  let b ← s.runExternal encoded
+  -- TODO: fix
+  return mkApp6 (.const ``Solver.correct []) cnfType certType
     (.const solverName []) (toExpr c) (toExpr b) (← mkEqRefl (toExpr true))
 
 -- We can solve *very* small problems by decidability.
-def byDecideSolver : Solver Unit where
+def byDecideSolver : Solver (CNF Nat) Unit where
+  encodeCNF := id
   runExternal _ := pure ()
   verify c _ := decide c.unsat
   correct _ _ := of_decide_eq_true
 
 def byDecideSolver' : CNF Nat → MetaM Expr :=
-  Solver.lift ``byDecideSolver (.const ``Unit []) byDecideSolver
+  Solver.lift ``byDecideSolver (mkApp (mkConst ``CNF) (mkConst ``Nat)) (.const ``Unit []) byDecideSolver
 
 def _root_.Lean.MVarId.cnfDecide (g : MVarId) : MetaM Unit := M.run do
   let g' ← falseOrByContra g
