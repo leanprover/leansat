b8fa7b2400a7faaaa44b9ee518fbc30a8cb4e17c
Scott Morrison <scott.morrison@gmail.com>
2024-02-26 11:34:41 +1100

    .

--- LeanSAT/Reflect/CNF/Relabel.lean
+++ LeanSAT/Reflect/CNF/Relabel.lean
@@ -11,6 +11,9 @@ set_option linter.missingDocs false
     (List.replicate n a).map f = List.replicate n (f a) := by
   induction n <;> simp_all
 
+theorem _root_.List.map_congr {f g : α → β} {L : List α} (w : ∀ a, a ∈ L → f a = g a) :
+    L.map f = L.map g := sorry
+
 namespace CNF
 
 namespace Clause
@@ -20,6 +23,23 @@ def relabel (f : α → β) (c : Clause α) : Clause β := c.map (fun (i, n) => 
 @[simp] theorem eval_relabel {f : α → β} {g : β → Bool} {x : Clause α} :
     (relabel f x).eval g = x.eval (g ∘ f) := by
   induction x <;> simp_all [relabel]
+
+@[simp] theorem relabel_id' : relabel (id : α → α) = id := by funext; simp [relabel]
+
+theorem relabel_congr {x : Clause α} {f g : α → β} (w : ∀ a, mem a x → f a = g a) :
+    relabel f x = relabel g x := by
+  simp [relabel]
+  rw [List.map_congr]
+  intro ⟨i, b⟩ h
+  congr
+  apply w
+
+
+-- We need the unapplied equality later.
+@[simp] theorem relabel_relabel' : relabel f ∘ relabel g = relabel (f ∘ g) := by
+  funext i
+  simp only [Function.comp_apply, relabel, List.map_map]
+  rfl
 
 end Clause
 
@@ -37,6 +57,14 @@ def relabel (f : α → β) (g : CNF α) : CNF β := g.map (Clause.relabel f)
 
 @[simp] theorem relabel_append : relabel f (g ++ h) = relabel f g ++ relabel f h :=
   List.map_append _ _ _
+
+@[simp] theorem relabel_relabel : relabel f (relabel g x) = relabel (f ∘ g) x := by
+  simp only [relabel, List.map_map, Clause.relabel_relabel']
+
+@[simp] theorem relabel_id : relabel id x = x := by simp [relabel]
+
+theorem relabel_congr {x : CNF α} {f g : α → β} (w : ∀ a, mem a x → f a = g a) :
+  relabel f x = relabel g x := sorry -- define `Clause.relabel_congr` first
 
 theorem sat_relabel (h : sat x (g ∘ f)) : sat (relabel f x) g := by
   simp_all [sat]
@@ -56,28 +84,26 @@ theorem nonempty_or_impossible (x : CNF α) : Nonempty α ∨ ∃ n, x = List.re
         exact ⟨n + 1, rfl⟩
     | ⟨a, b⟩ :: c => exact Or.inl ⟨a⟩
 
-theorem unsat_relabel_iff {x : CNF α} {f : α → β} (w : ∀ a b, f a = f b → a = b) :
+theorem unsat_relabel_iff {x : CNF α} {f : α → β}
+    (w : ∀ {a b}, mem a x → mem b x → f a = f b → a = b) :
     unsat (relabel f x) ↔ unsat x := by
   rcases nonempty_or_impossible x with (⟨⟨a₀⟩⟩ | ⟨n, rfl⟩)
   · refine ⟨fun h => ?_, unsat_relabel f⟩
     have em := Classical.propDecidable
     let g : β → α := fun b =>
-      if h : ∃ a, f a = b then h.choose else a₀
+      if h : ∃ a, mem a x ∧ f a = b then h.choose else a₀
     have h' := unsat_relabel g h
     suffices w : relabel g (relabel f x) = x by
       rwa [w] at h'
-    simp only [relabel, List.map_map]
-    suffices w : Clause.relabel _ ∘ Clause.relabel _ = id by
-      rw [w, List.map_id]
-    funext c
-    simp only [Function.comp_apply, id_eq, Clause.relabel, List.map_map]
-    suffices w : _ ∘ _ = id by
-      rw [w, List.map_id]
-    funext ⟨a, b⟩
-    simp only [Function.comp_apply, exists_apply_eq_apply, ↓reduceDite, id_eq]
-    congr
-    apply w
-    exact Exists.choose_spec (⟨a, rfl⟩ : ∃ a', f a' = f a)
+    have : ∀ a, mem a x → g (f a) = a := by
+      intro a h
+      dsimp [g]
+      rw [dif_pos ⟨a, h, rfl⟩]
+      apply w _ h
+      · exact (Exists.choose_spec (⟨a, h, rfl⟩ : ∃ a', mem a' x ∧ f a' = f a)).2
+      · exact (Exists.choose_spec (⟨a, h, rfl⟩ : ∃ a', mem a' x ∧ f a' = f a)).1
+    rw [relabel_relabel, relabel_congr, relabel_id]
+    exact this
   · cases n <;> simp [unsat, relabel, Clause.relabel]
 
 end CNF
