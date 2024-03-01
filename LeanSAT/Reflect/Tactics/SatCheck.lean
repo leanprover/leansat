e99fdfb2eefd648b21b8a818646ed03bcd8b8033
Henrik Böving <hargonix@gmail.com>
2024-03-01 13:44:51 +0100

    feat: sat_decide? with code action

--- LeanSAT/Reflect/Tactics/SatCheck.lean
+++ LeanSAT/Reflect/Tactics/SatCheck.lean
@@ -4,25 +4,26 @@ open Lean Elab Meta ReflectSat
 
 namespace SatCheck
 
-structure TacticContext extends SatDecide.TacticContext where
-  lratPath : System.FilePath
-
-def TacticContext.new (lratPath : System.FilePath) : TermElabM TacticContext := do
-  let inner ← SatDecide.TacticContext.new
-  -- Figure out where the current .lean file is to look for the LRAT file in the same directory
+/--
+Get the directory that contains the Lean file which is currently being elaborated.
+-/
+def getSrcDir : TermElabM System.FilePath := do
   let ctx ← readThe Lean.Core.Context
   let srcPath := System.FilePath.mk ctx.fileName
   let some srcDir := srcPath.parent
     | throwError "cannot compute parent directory of '{srcPath}'"
-  let lratPath := srcDir / lratPath
-  return TacticContext.mk inner lratPath
+  return srcDir
+
+def mkContext (lratPath : System.FilePath) : TermElabM SatDecide.TacticContext := do
+  let lratPath := (← getSrcDir) / lratPath
+  SatDecide.TacticContext.new lratPath
 
 /--
 Prepare an `Expr` that proofs `boolExpr.unsat` using `ofReduceBool`.
 -/
-def lratChecker (cfg : TacticContext) (boolExpr : BoolExprNat) : MetaM Expr := do
+def lratChecker (cfg : SatDecide.TacticContext) (boolExpr : BoolExprNat) : MetaM Expr := do
   let cert ← SatDecide.LratCert.ofFile cfg.lratPath
-  cert.toReflectionProof cfg.toTacticContext boolExpr
+  cert.toReflectionProof cfg boolExpr
 
 /--
 Close a goal by:
@@ -35,19 +36,19 @@ It is called with the name of an LRAT file in the same directory as the current 
 sat_check "proof.lrat"
 ```
 -/
-def _root_.Lean.MVarId.satCheck (g : MVarId) (cfg : TacticContext) : MetaM Unit := do
+def _root_.Lean.MVarId.satCheck (g : MVarId) (cfg : SatDecide.TacticContext) : MetaM Unit := do
   let unsatProver (exp : BoolExprNat) : MetaM Expr := do
     withTraceNode `sat (fun _ => return "Preparing LRAT reflection term") do
       lratChecker cfg exp
   g.closeWithBoolReflection unsatProver
 
 @[inherit_doc Lean.MVarId.satCheck]
-syntax (name := satCheckSyntax) "sat_check" str : tactic
+syntax (name := satCheckSyntax) "sat_check " str : tactic
 
 end SatCheck
 
 open Elab.Tactic
 elab_rules : tactic
   | `(tactic| sat_check $path:str) => do
-    let cfg ← SatCheck.TacticContext.new path.getString
+    let cfg ← SatCheck.mkContext path.getString
     liftMetaFinishingTactic fun g => g.satCheck cfg
---
+++ LeanSAT/Reflect/Tactics/SatTrace.lean
@@ -0,0 +1,38 @@
+import LeanSAT.Reflect.Tactics.SatDecide
+import LeanSAT.Reflect.Tactics.SatCheck
+import Std.Tactic.TryThis
+
+open Lean Elab Meta Tactic
+
+namespace SatTrace
+
+/--
+Suggest a `sat_check` invocation for a `sat_decide` tactic call.
+Useful for caching LRAT proofs.
+-/
+syntax (name := satTraceSyntax) "sat_decide?" : tactic
+
+-- TODO: think of a more maintainable file pattern for this stuff.
+/--
+Produce a file with the pattern:
+LeanFileName-DeclName-Line-Col.lrat
+-/
+def getLratFileName : TermElabM System.FilePath := do
+  let some baseName := System.FilePath.mk (← getFileName) |>.fileName | throwError "could not find file name"
+  let some declName ← Term.getDeclName? | throwError "could not find declaration name"
+  let pos := (← getFileMap).toPosition (← getRefPos)
+  return s!"{baseName}-{declName}-{pos.line}-{pos.column}.lrat"
+
+@[tactic satTraceSyntax, inherit_doc satTraceSyntax]
+def evalSatTraceTactic : Tactic := fun stx =>
+  match stx with
+  | `(tactic| sat_decide?%$tk) => do
+    let lratFile : System.FilePath ← getLratFileName
+    let cfg ← SatCheck.mkContext lratFile
+    (← getMainGoal).withContext do
+      liftMetaFinishingTactic fun g => g.satDecide cfg
+    let stx ← `(tactic| sat_check $(quote lratFile.toString))
+    Std.Tactic.TryThis.addSuggestion tk stx (origSpan? := ← getRef)
+  | _ => throwUnsupportedSyntax
+
+end SatTrace
