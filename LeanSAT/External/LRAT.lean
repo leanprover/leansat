f2c2298579512e3e459e0e0b653c1622e8563424
Henrik Böving <hargonix@gmail.com>
2024-04-03 14:21:05 +0200

    perf: optimize the LRAT parser

--- LeanSAT/External/LRAT.lean
+++ LeanSAT/External/LRAT.lean
@@ -6,145 +6,140 @@ Authors: Josh Clune
 import Lean.Elab.Command
 import LeanSAT.LRAT.Actions
 
-open Lean Parser Elab Command
+open Lean Elab Command
 
 namespace LRAT
-
-def makeDeletionArr (tokens : Array String) : Option (Array Nat) := do
-  let mut encounteredError := false
-  let mut deletionArr : Array Nat := #[]
-  for token in tokens do
-    if token == "" then continue
-    match String.toNat? token with
-    | some id => deletionArr := deletionArr.push id
-    | none =>
-      encounteredError := true
-      break
-  if encounteredError then none
-  else some deletionArr
-
-def makeDeletion (tokens : Array String) : Option IntAction :=
-  match makeDeletionArr tokens with
-  | some deletionArr => some $ .del deletionArr
-  | none => none
-
-def makeClause (clauseTokens : Array String) : Option (Array Int) := do
-  let mut encounteredError := false
-  let mut clause : Array Int := #[]
-  for token in clauseTokens do
-    if token == "" then continue
-    match String.toInt? token with
-    | some l => clause := clause.push l
-    | none =>
-      encounteredError := true
-      break
-  if encounteredError then none
-  else some clause
-
-def makeHints (hintTokens : Array String) : Option (Array Nat × Array (Nat × Array Nat)) := do
-  if hintTokens.size == 0 then none
-  else
-    let mut rupHints : Array Nat := #[]
-    let mut ratHints : Array (Nat × Array Nat) := #[]
-
-    let mut encounteredError := false
-    let mut processingRupHint := true
-    let mut curRatHint : Option (Nat × Array Nat) := none
-    for hint in hintTokens do
-      match hint.toInt? with
-      | some hint =>
-        if hint < 0 then -- Starting a new ratHint
-          if processingRupHint then
-            processingRupHint := false -- Finished processingRupHint
-            curRatHint := some (hint.natAbs, #[])
-          else
-            match curRatHint with
-            | none => -- Nothing to push to ratHints because this is the first ratHint
-              curRatHint := some (hint.natAbs, #[])
-            | some prevRatHint =>
-              ratHints := ratHints.push prevRatHint
-              curRatHint := some (hint.natAbs, #[])
-        else
-          if processingRupHint then
-            rupHints := rupHints.push hint.natAbs -- hint.natAbs = hint because hint > 0
-          else
-            match curRatHint with
-            | some (curRatId, curRatArr) => curRatHint := (curRatId, curRatArr.push hint.natAbs)
-            | none => -- This case shouldn't be possible
-              encounteredError := true
-              break
-      | none =>
-        encounteredError := true
-        break
-    -- Still have to add final ratHint since it is not followed by a negative number
-    ratHints :=
-      match curRatHint with
-      | none => ratHints
-      | some finalRatHint => ratHints.push finalRatHint
-    if encounteredError then none
-    else some (rupHints, ratHints)
 
 def getPivot (clause : Array Int) : Literal Nat :=
   let pivotInt := clause[0]!
   if pivotInt > 0 then (pivotInt.natAbs, true)
   else (pivotInt.natAbs, false)
 
-def makeAddition (clauseId : Nat) (clauseTokens : Array String) (hintTokens : Array String) : Option IntAction := do
-  match makeClause clauseTokens, makeHints hintTokens with
-  | some clause, some (rupHints, ratHints) =>
-    match clause.size, ratHints.size with
-    | 0, 0 => some $ .addEmpty clauseId rupHints
-    | 0, _ => none -- There cannot be any ratHints for adding the empty clause because the empty clause has no pivot
-    | _, 0 => some $ .addRup clauseId clause rupHints
-    | _, _ => some $ .addRat clauseId clause (getPivot clause) rupHints ratHints
-  | _, _ => none
+namespace Parser
 
-/-- `lineToActionCommand` takes in a single unparsed LRAT line and attempts to parse it into an `IntAction`.
-    `lineToActionCommand` will fail if the given line is not a complete unparsed LRAT line or if a syntax error
-    is found in the line.
+/-
+This implements a (correct) version of the grammar presented in:
+https://www.cs.cmu.edu/~mheule/publications/lrat.pdf
+-/
 
-    `lineToActionCommand` is written in a `CommandElabM` monad so that it can throw errors on failure and be called
-    by `loadLRATProof`-/
-def lineToActionCommand (line : String) : CommandElabM IntAction := do
-  let mut clauseID : Option Nat := none
-  let mut firstSectionTokens : Array String := #[]
-  let mut hintTokens : Array String := #[]
-  let mut isDeletion : Option Bool := none -- none represents unknown
-  let mut numZeroes := 0
+open Parsec
 
-  let line := line.replace "\t" " "
-  let lineAction := (line.splitOn " ").toArray
-  for token in lineAction do
-    match clauseID with
-    | none =>
-      match String.toNat? token with
-      | some id => clauseID := some id
-      | none => throwError "Unable to parse id"
-    | some id =>
-      if firstSectionTokens == #[] && isDeletion == none then
-        if token == "d" then
-          isDeletion := some true
-          continue -- d is not part of the clause and therefore shouldn't be added to firstSectionTokens
-        else
-          isDeletion := some false -- no continue because we should add token to firstSectionTokens
-      if token == "0" then
-        match isDeletion with
-        | none => throwError "Error while loading LRAT proof"
-        | some true =>
-          match makeDeletion firstSectionTokens with
-          | none => throwError "Error while loading LRAT proof"
-          | some action => return action
-        | some false =>
-          if numZeroes == 0 then -- This zero ends the clause, but not the action
-            numZeroes := 1
-          else -- This zero ends the hint section
-            match makeAddition id firstSectionTokens hintTokens with
-            | none => throwError "Error while loading LRAT proof"
-            | some action => return action
+@[inline]
+def tryCatch (p : Parsec α)
+    (csuccess : α → Parsec β)
+    (cerror : Unit → Parsec β)
+    : Parsec β := fun it =>
+  match p it with
+  | .success rem a => csuccess a rem
+  | .error rem err =>
+    -- We assume that it.s never changes as the `Parsec` monad only modifies `it.pos`.
+    if it.pos = rem.pos then cerror () rem else .error rem err
+
+-- TODO: upstream, linearity bug in manyCore orig impl
+@[specialize]
+partial def manyCore (p : Parsec α) (acc : Array α) : Parsec $ Array α :=
+  tryCatch p (manyCore p <| acc.push ·) (fun _ => pure acc)
+
+-- TODO: delete after manyCore upstream
+@[inline]
+def many (p : Parsec α) : Parsec $ Array α := manyCore p #[]
+
+-- TODO: delete after manyCore upstream
+@[inline]
+def many1 (p : Parsec α) : Parsec $ Array α := do manyCore p #[←p]
+
+/--
+Assumes `c` is between `0` and `9`
+-/
+def digitToNat (c : Char) : Nat := c.toNat - '0'.toNat
+
+def digitsToNat (digits : Array Nat) : Nat :=
+  digits.foldl (fun r d => r * 10 + d) 0
+
+def parsePos : Parsec Nat := do
+  let digits ← many1 (digitToNat <$> digit)
+  let ident := digitsToNat digits
+  if ident == 0 then
+    fail "id was 0"
+  else
+    return ident
+
+def parseNeg : Parsec Int := do
+  skipChar '-'
+  let nat ← parsePos
+  return -nat
+
+def parseId : Parsec Nat := parsePos
+
+def parseZero : Parsec Unit := skipChar '0'
+
+def parseIdList : Parsec (Array Nat) := do
+  many idWs
+where
+  idWs : Parsec Nat := do
+    let ident ← attempt parseId
+    ws
+    return ident
+
+def parseDelete (_ident : Nat) : Parsec IntAction := do
+  skipChar 'd'
+  ws
+  let idList ← parseIdList
+  parseZero
+  return .del idList
+
+def parseLit : Parsec Int := do
+  parseNeg <|> (Int.ofNat <$> parsePos)
+
+def parseClause : Parsec (Array Int) := do
+  let lits ← many litWs
+  parseZero
+  return lits
+where
+  litWs : Parsec Int := do
+    let lit ← attempt parseLit
+    ws
+    return lit
+
+def parseRes : Parsec (Nat × Array Nat) := do
+  let lhs ← parseNeg
+  ws
+  let idents ← parseIdList
+  return (lhs.natAbs, idents)
+
+def parseRat (ident : Nat) : Parsec IntAction := do
+  let clause ← parseClause
+  ws
+  let rupHints ← parseIdList
+  let ratHints ← many (attempt parseRes)
+  parseZero
+  match clause.size, ratHints.size with
+  | 0, 0 => return .addEmpty ident rupHints
+  | 0, _ => fail "There cannot be any ratHints for adding the empty clause"
+  | _, 0 => return .addRup ident clause rupHints
+  | _, _ => return .addRat ident clause (getPivot clause) rupHints ratHints
+
+def parseLine : Parsec IntAction := do
+  let ident ← parseId
+  ws
+  parseDelete ident <|> parseRat ident
+
+partial def parseLines : Parsec (Array IntAction) :=
+  go #[]
+where
+  go (actions : Array IntAction) : Parsec (Array IntAction) := do
+    let actions ←
+      if (← peek!) == 'c' then
+        let _ ← many (satisfy (· != '\n'))
+        skipChar '\n'
+        pure actions
       else
-        if numZeroes == 0 then firstSectionTokens := firstSectionTokens.push token
-        else hintTokens := hintTokens.push token
-  throwError "End of line should correspond with end of LRAT action"
+        let action ← parseLine
+        skipChar '\n'
+        pure (actions.push action)
+    (eof *> pure actions) <|> go actions
+
+end Parser
 
 /-- `loadLRATProof` takes in the path of an LRAT proof and attempts to output an Array of IntActions
     that correspond to the parsed LRAT proof.
@@ -152,82 +147,26 @@ def lineToActionCommand (line : String) : CommandElabM IntAction := do
     `loadLRATProof` is written as a `CommandElabM` monad so that it can be used in commands such as `loadLRAT` at
     the end of this file. -/
 def loadLRATProof (path : System.FilePath) : CommandElabM (Array IntAction) := do
-  let lines ← IO.FS.lines path
-  let lines := lines.filter fun l => not (l.startsWith "c")
-  let mut proof : Array IntAction := #[]
-  for line in lines do
-    proof := proof.push $ ← lineToActionCommand line
-  return proof
+  let proof ← IO.FS.readFile path
+  match Parser.parseLines |>.run proof with
+  | .ok actions => return actions
+  | .error err => throwError err
 
-/-- `lineToAction` takes in a single unparsed LRAT line and attempts to parse it into an `IntAction`.
-    `lineToAction` will fail if the given line is not a complete unparsed LRAT line or if a syntax error
-    is found in the line.
+def lineToAction (line : String) : Option IntAction :=
+  match Parser.parseLine |>.run line.trim with
+  | .ok action => some action
+  | .error .. => none
 
-   `lineToAction` is written in an `IO` monad so that it can print errors on failure. `lineToAction` is called
-   in `readLRATProof` but can also be called independently by a program such as `main` to facilitate parsing an
-   LRAT proof one line at a time and checking the LRAT proof incrementally with `incrementalLRATChecker` -/
-def lineToAction (line : String) : Option IntAction := Id.run do
-  let mut encounteredError := false
-  let mut action : Option IntAction := none
-
-  let mut clauseID : Option Nat := none
-  let mut firstSectionTokens : Array String := #[]
-  let mut hintTokens : Array String := #[]
-  let mut isDeletion : Option Bool := none -- none represents unknown
-  let mut numZeroes := 0
-
-  let line := line.replace "\t" " "
-  let line := line.replace "\n" ""
-  let lineAction := (line.splitOn " ").toArray
-  for token in lineAction do
-    if action != none then
-      encounteredError := true
-      break
-    match clauseID with
-    | none =>
-      match String.toNat? token with
-      | some id => clauseID := some id
-      | none => encounteredError := true; break
-    | some id =>
-      if firstSectionTokens == #[] && isDeletion == none then
-        if token == "d" then
-          isDeletion := some true
-          continue -- d is not part of the clause and therefore shouldn't be added to firstSectionTokens
-        else
-          isDeletion := some false -- no continue because we should add token to firstSectionTokens
-      if token == "0" then
-        match isDeletion with
-        | none => encounteredError := true; break
-        | some true =>
-          match makeDeletion firstSectionTokens with
-          | none => encounteredError := true; break
-          | some curAction => action := some curAction
-        | some false =>
-          if numZeroes == 0 then -- This zero ends the clause, but not the action
-            numZeroes := 1
-          else -- This zero ends the hint section
-            match makeAddition id firstSectionTokens hintTokens with
-            | none =>
-              encounteredError := true
-              break
-            | some curAction => action := some curAction
-      else
-        if numZeroes == 0 then firstSectionTokens := firstSectionTokens.push token
-        else hintTokens := hintTokens.push token
-  if encounteredError then return none
-  else return action
-
-def parseLRATProof (lines : Array String) : Option (Array IntAction) := Id.run do
-  let lines := lines.filter fun l => not (l.startsWith "c")
-  let mut proof : Array IntAction := #[]
-  for line in lines do
-    match lineToAction line with
-    | some action => proof := proof.push action
-    | none => return none
-  return some proof
+set_option trace.compiler.ir.result true in
+def parseLRATProof (proof : String) : Option (Array IntAction) := Id.run do
+  match Parser.parseLines |>.run proof with
+  | .ok actions => return some actions
+  | .error .. => return none
 
 /-- `readLRATProof` takes in the path of an LRAT proof and attempts to output an Array of IntActions
     that correspond to the parsed LRAT proof. -/
 def readLRATProof (path : System.FilePath) : IO (Option (Array IntAction)) := do
-  let lines ← IO.FS.lines path
-  return parseLRATProof lines
+  let proof ← IO.FS.readFile path
+  match Parser.parseLines |>.run proof with
+  | .ok actions => return some actions
+  | .error .. => return none
