4f0ce5b4b258e47b25192b65985ec8f04b6101f2
Henrik Böving <hargonix@gmail.com>
2024-04-05 11:50:22 +0200

    chore: increase stack size for LSP

--- lakefile.lean
+++ lakefile.lean
@@ -5,6 +5,8 @@ require std from git "https://github.com/leanprover/std4.git"@"main"
 
 package LeanSAT {
   precompileModules := true
+  -- This is 32 MB stack size for threads, we need this for bitblasting very large formulas
+  moreGlobalServerArgs := #["--tstack=32000"]
 }
 
 lean_lib Test
