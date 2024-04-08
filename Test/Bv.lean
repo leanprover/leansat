5bd5a1fc15655a3ec7afa1a053a6e13eb45d7427
Henrik Böving <hargonix@gmail.com>
2024-04-08 11:01:44 +0200

    feat: sorry free bv_decide

--- Test/Bv.lean
+++ Test/Bv.lean
@@ -10,3 +10,16 @@ theorem foo (h1 : 0#1 = 1#1) (h2 : (2 : BitVec 2) = 3) (h3 : x = 4#3)
 set_option trace.bv true in
 theorem all_features {x y : BitVec 256} (h : x = y) : (~~~x) &&& 1 = (~~~y) &&& 1#256 := by
   bv_decide
+
+theorem axiomCheck (x : BitVec 1) : x = x := by bv_decide
+
+/--
+info: 'axiomCheck' depends on axioms: [propext,
+ Quot.sound,
+ Classical.choice,
+ AIG.RelabelNat.State.Inv1.property,
+ AIG.RelabelNat.State.Inv2.property,
+ Lean.ofReduceBool]
+-/
+#guard_msgs in
+#print axioms axiomCheck
