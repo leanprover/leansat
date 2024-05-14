5f354c2237ed15c48f5fb253ce7f888604ef6a69
Henrik Böving <hargonix@gmail.com>
2024-05-14 11:56:29 +0200

    refactor: reduce output of check_eval

--- check_eval.bash
+++ check_eval.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -x
 export LC_ALL=C
 
 LIB="Eval"
@@ -16,6 +15,7 @@ ulimit -s unlimited
 # Build every file and memorize the results in run_[success|fail].txt
 for path in $(find "${LIB}" -type f  -name "*.lean" | sort); do
     file_name_wo_suffix=$(basename -s .lean $path)
+    echo "Trying $LIB.${file_name_wo_suffix}"
     lake build $LIB.${file_name_wo_suffix} > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo $(basename $path) >> $RUN_SUCCESSES
