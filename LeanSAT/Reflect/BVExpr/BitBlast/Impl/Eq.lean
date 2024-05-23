a3517e8999d0a392033f9f437007958ab9f2159a
Henrik Böving <hargonix@gmail.com>
2024-05-23 13:33:44 +0200

    refactor: split up mkEq

--- LeanSAT/Reflect/BVExpr/BitBlast/Impl/Eq.lean
+++ LeanSAT/Reflect/BVExpr/BitBlast/Impl/Eq.lean
@@ -12,41 +12,50 @@ def mkEq (aig : AIG BVBit) (pair : ExprPair) : AIG.Entrypoint BVBit :=
   let lhsRefs := lhsRefs.cast <| by
     simp (config := { zetaDelta := true }) only
     apply AIG.LawfulStreamOperator.le_size (f := BVExpr.bitblast)
-  let res := AIG.RefStream.zip aig ⟨⟨lhsRefs, rhsRefs⟩, AIG.mkBEqCached⟩
-  let aig := res.aig
-  let bits := res.stream
-  AIG.RefStream.fold aig (.mkAnd bits)
+  streamProc aig ⟨lhsRefs, rhsRefs⟩
+where
+  streamProc {w : Nat} (aig : AIG BVBit) (input : AIG.BinaryRefStream aig w) : AIG.Entrypoint BVBit :=
+    let res := AIG.RefStream.zip aig ⟨input, AIG.mkBEqCached⟩
+    let aig := res.aig
+    let bits := res.stream
+    AIG.RefStream.fold aig (.mkAnd bits)
 
-theorem mkEq_le_size (aig : AIG BVBit) (target : ExprPair)
-    : aig.decls.size ≤ (mkEq aig target).aig.decls.size := by
-  unfold mkEq
-  dsimp
-  apply AIG.LawfulOperator.le_size_of_le_aig_size (f := AIG.RefStream.fold)
-  apply AIG.LawfulStreamOperator.le_size_of_le_aig_size (f := AIG.RefStream.zip)
-  apply AIG.LawfulStreamOperator.le_size_of_le_aig_size (f := BVExpr.bitblast)
-  apply AIG.LawfulStreamOperator.le_size (f := BVExpr.bitblast)
-
-theorem mkEq_decl_eq (aig : AIG BVBit) (target : ExprPair) {h : idx < aig.decls.size} :
-    have := mkEq_le_size aig target
-    (mkEq aig target).aig.decls[idx]'(by omega) = aig.decls[idx]'h := by
-  unfold mkEq
-  dsimp
-  rw [AIG.LawfulOperator.decl_eq]
-  rw [AIG.LawfulStreamOperator.decl_eq]
-  rw [AIG.LawfulStreamOperator.decl_eq (f := BVExpr.bitblast)]
-  rw [AIG.LawfulStreamOperator.decl_eq (f := BVExpr.bitblast)]
-  . apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
-    assumption
-  . apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
-    apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
-    assumption
-  . apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := AIG.RefStream.zip)
-    apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
-    apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
+instance {w : Nat} : AIG.LawfulOperator BVBit (AIG.BinaryRefStream · w) mkEq.streamProc where
+  le_size := by
+    intros
+    unfold mkEq.streamProc
+    dsimp
+    apply AIG.LawfulOperator.le_size_of_le_aig_size (f := AIG.RefStream.fold)
+    apply AIG.LawfulStreamOperator.le_size (f := AIG.RefStream.zip)
+  decl_eq := by
+    intros
+    unfold mkEq.streamProc
+    dsimp
+    rw [AIG.LawfulOperator.decl_eq (f := AIG.RefStream.fold)]
+    rw [AIG.LawfulStreamOperator.decl_eq (f := AIG.RefStream.zip)]
+    apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := AIG.RefStream.zip)
     assumption
 
 instance : AIG.LawfulOperator BVBit (fun _ => ExprPair) mkEq where
-  le_size := mkEq_le_size
-  decl_eq := by intros; apply mkEq_decl_eq
+  le_size := by
+    intros
+    unfold mkEq
+    dsimp
+    apply AIG.LawfulOperator.le_size_of_le_aig_size (f := mkEq.streamProc)
+    apply AIG.LawfulStreamOperator.le_size_of_le_aig_size (f := BVExpr.bitblast)
+    apply AIG.LawfulStreamOperator.le_size (f := BVExpr.bitblast)
+  decl_eq := by
+    intros
+    unfold mkEq
+    dsimp
+    rw [AIG.LawfulOperator.decl_eq (f := mkEq.streamProc)]
+    rw [AIG.LawfulStreamOperator.decl_eq (f := BVExpr.bitblast)]
+    rw [AIG.LawfulStreamOperator.decl_eq (f := BVExpr.bitblast)]
+    . apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
+      assumption
+    . apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
+      apply AIG.LawfulStreamOperator.lt_size_of_lt_aig_size (f := BVExpr.bitblast)
+      assumption
+
 
 end BVPred
