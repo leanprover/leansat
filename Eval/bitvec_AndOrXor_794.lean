39fde13806f0b27fdf92869783aebd1676e02467
Henrik Böving <hargonix@gmail.com>
2024-05-23 14:55:41 +0200

    test: adjust eval test suite

--- Eval/bitvec_AndOrXor_794.lean
+++ Eval/bitvec_AndOrXor_794.lean
@@ -1,5 +1,5 @@
 import LeanSAT.Reflect.Tactics.BVDecide
 
 theorem bitvec_AndOrXor_794 :
- ∀ (a b : BitVec 64), (BitVec.slt b a) && (a != b) = (BitVec.slt b a)
+ ∀ (a b : BitVec 64), ((BitVec.slt b a) && (a != b)) = (BitVec.slt b a)
 := by intros; bv_decide
