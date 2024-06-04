a9ebec30972da29db4d8d1a8eabc5f61f8085300
Henrik Böving <hargonix@gmail.com>
2024-06-04 14:54:01 +0200

    test: limit bit width of mul eval problems and add new successes

--- Eval/bitvec_290__292.lean
+++ Eval/bitvec_290__292.lean
@@ -2,5 +2,5 @@ import LeanSAT.Reflect.Tactics.BVDecide
 open BitVec
 
 theorem bitvec_290__292 :
- ∀ (Y Op1 : BitVec 64), 1#64 <<< Y * Op1 = Op1 <<< Y
+ ∀ (Y Op1 : BitVec 8), 1#8 <<< Y * Op1 = Op1 <<< Y
 := by intros; bv_decide
