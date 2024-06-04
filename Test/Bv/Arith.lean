8e138f1b01fea2f6f183b90034a430b9d2316c00
Henrik Böving <hargonix@gmail.com>
2024-06-04 14:46:51 +0200

    test: BitVec.mul

--- Test/Bv/Arith.lean
+++ Test/Bv/Arith.lean
@@ -21,3 +21,15 @@ theorem arith_unit_2' (x y : BitVec 64) : BitVec.sub x y = (BitVec.neg y) + x :=
 set_option trace.bv true in
 theorem arith_unit_3 (x y : BitVec 16) : x - (x - y) = y := by
   bv_decide
+
+set_option trace.bv true in
+theorem arith_unit_4 (x y : BitVec 4) : x * y = y * x := by
+  bv_decide
+
+set_option trace.bv true in
+theorem arith_unit_5 (x : BitVec 4) : x * 32 = 32 * x := by
+  bv_decide
+
+set_option trace.bv true in
+theorem arith_unit_6 (x : BitVec 64) : x + x = 2 * x := by
+  bv_decide
