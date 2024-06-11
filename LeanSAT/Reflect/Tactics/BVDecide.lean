b8516449d605ff99e0ac8b6a976b10670bb29f40
Henrik Böving <hargonix@gmail.com>
2024-06-11 16:51:33 +0200

    chore: minimize imports after refactor

--- LeanSAT/Reflect/Tactics/BVDecide.lean
+++ LeanSAT/Reflect/Tactics/BVDecide.lean
@@ -3,14 +3,10 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Henrik Böving
 -/
-import LeanSAT.Reflect.BVExpr.Basic
 import LeanSAT.Reflect.BVExpr.BitBlast
 import LeanSAT.Reflect.Tactics.Normalize
 import LeanSAT.Reflect.LRAT
-
-import LeanSAT.LRAT.LRATChecker
-import LeanSAT.LRAT.LRATCheckerSound
-import LeanSAT.External.Solver
+import LeanSAT.AIG.CNF
 import LeanSAT.AIG.RelabelNat
 
 open Lean Meta
