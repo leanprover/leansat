f3175cf309dc8ba92497ad9b6ec2056460158fbc
Henrik Böving <hargonix@gmail.com>
2024-06-11 16:13:05 +0200

    chore: migrate all sat tests to bv

---
+++ Test/Bv/Sat.lean
@@ -0,0 +1,11 @@
+import LeanSAT.Reflect.Tactics.BVDecide
+import LeanSAT.Reflect.Tactics.BVTrace
+
+example (h : true = false) : False := by bv_decide
+example {x y z : Bool} (_ : (x && y) = z) (_ : x = !y) (_ : z = true) : False := by bv_decide
+example {a b c d e f : Bool} (_ : (a && b) = c) (_ : (b && c) = d) (_ : (c && d) = e) (_ : (d && e) = f)
+    (_ : a = false) (_ : f = true) : False := by bv_decide
+
+example (h : true = false) : False := by bv_decide
+example (h : x = false) : false = x := by bv_decide
+example (h : x = false) : false = x := by bv_check "sat_check.lrat"
