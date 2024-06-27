cc456c46822b61db801895c1f7347509038a555b
Henrik Böving <hargonix@gmail.com>
2024-06-27 10:52:05 +0200

    test: arbitrary shiftLeft implementation

--- Test/Eval/bitvec_InstCombineShift__497'''.lean
+++ Test/Eval/bitvec_InstCombineShift__497'''.lean
@@ -1,5 +1,5 @@
 import LeanSAT.Tactics.BVDecide
 
 theorem bitvec_InstCombineShift__497''' :
- ∀ (X C C2 : BitVec 64), (X + C2) <<< C = X <<< C + C2 <<< C
+ ∀ (X C C2 : BitVec 32), (X + C2) <<< C = X <<< C + C2 <<< C
 := by intros; bv_decide
