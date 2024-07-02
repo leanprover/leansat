4e4ba4a82a985efd29f42e8e33da60d8405098c3
Henrik Böving <hargonix@gmail.com>
2024-07-02 17:34:41 +0200

    feat: finish will overflow

--- Eval/WillOverflow.lean
+++ Eval/WillOverflow.lean
@@ -8,7 +8,26 @@ open BitVec
 
 namespace DerivingBitTwiddlingHack
 
--- TODO: add version with ifs
+
+/-
+bool will_add_overflow_64bit(int32_t a, int32_t b) {
+    // a and b are promoted to 64-bit signed integers
+    int64_t result = (int64_t)a + b;
+    if (result < INT32_MIN || result > INT32_MAX) {
+        return true;
+    }
+    return false;
+}
+-/
+
+def will_add_overflow_64bit (a b : BitVec 32) : Bool :=
+  let a := a.signExtend 64
+  let b := b.signExtend 64
+  let result := a + b
+  -- copied from libc
+  let INT32_MAX := 0x7fffffff#32
+  let INT32_MIN := (-0x7fffffff#32 - 1#32)
+  BitVec.slt result (INT32_MIN.signExtend 64) || BitVec.slt (INT32_MAX.signExtend 64) result
 
 /-
 bool will_add_overflow_expression(int32_t a_, int32_t b_) {
@@ -27,25 +46,8 @@ def will_add_overflow_expression (a b : BitVec 32) : Bool :=
     (x &&& 0x80000000#32) == 0x80000000#32
   (!NEG a && !NEG b && NEG c) || (NEG a && NEG b && !NEG c)
 
-/-
-bool will_add_overflow_bitwise(int32_t a_, int32_t b_) {
-    uint32_t a = (uint32_t)a_, b = (uint32_t)b_;
-    uint32_t c = (uint32_t)a + (uint32_t)b;
-    #define NEG(x) (((uint32_t)(x) & 0x80000000) == 0x80000000)
-    return ((!NEG(a) & !NEG(b) & NEG(c))
-        | (NEG(a) & NEG(b) & !NEG(c)));
-    #undef NEG
-}
--/
-
-def will_add_overflow_bitwise (a b : BitVec 32) : Bool :=
-  let c := a + b
-  let NEG (x : BitVec 32) : BitVec 1 :=
-    ofBool ((x &&& 0x80000000#32) == 0x80000000#32)
-  getLsb ((~~~NEG a &&& ~~~NEG b &&& NEG c) ||| (NEG a &&& NEG b &&& ~~~NEG c)) 0
-
-example (a b : BitVec 32) : will_add_overflow_expression a b = will_add_overflow_bitwise a b := by
-  dsimp [will_add_overflow_expression, will_add_overflow_bitwise]
+example (a b : BitVec 32) : will_add_overflow_64bit a b = will_add_overflow_expression a b := by
+  dsimp [will_add_overflow_64bit, will_add_overflow_expression]
   intros; bv_decide
 
 /-
@@ -59,12 +61,12 @@ bool will_add_overflow_bitwise_2(int32_t a_, int32_t b_) {
 -/
 def will_add_overflow_bitwise_2 (a b : BitVec 32) : Bool :=
   let c := a + b
-  let NEG (x : BitVec 32) : BitVec 1 :=
-    ofBool ((x &&& 0x80000000#32) == 0x80000000#32)
-  getLsb (NEG ((~~~a &&& ~~~b &&& c) ||| (a &&& b &&& ~~~c))) 0
+  let NEG (x : BitVec 32) : Bool :=
+    ((x &&& 0x80000000#32) == 0x80000000#32)
+  NEG ((~~~a &&& ~~~b &&& c) ||| (a &&& b &&& ~~~c))
 
-example (a b : BitVec 32) : will_add_overflow_bitwise a b = will_add_overflow_bitwise_2 a b := by
-  dsimp [will_add_overflow_bitwise, will_add_overflow_bitwise_2]
+example (a b : BitVec 32) : will_add_overflow_expression a b = will_add_overflow_bitwise_2 a b := by
+  dsimp [will_add_overflow_expression, will_add_overflow_bitwise_2]
   intros; bv_decide
 
 /-
@@ -107,7 +109,7 @@ bool will_add_overflow_optimized_b(int32_t a_, int32_t b_) {
 -/
 def will_add_overflow_optimized_b (a b : BitVec 32) : Bool :=
   let c := a + b
-  getLsb ((~~~(c ^^^ a) &&& (c ^^^ b)) >>> 31) 0
+  getLsb (((c ^^^ a) &&& (c ^^^ b)) >>> 31) 0
 
 example (a b : BitVec 32) :
     will_add_overflow_optimized_a a b = will_add_overflow_optimized_b a b := by
