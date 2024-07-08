6918b6346ca2d707c49642a442c99ab57cd53446
Henrik Böving <hargonix@gmail.com>
2024-07-08 13:27:30 +0200

    test: no more axioms are necessary

--- Test/Bv/AxiomCheck.lean
+++ Test/Bv/AxiomCheck.lean
@@ -7,11 +7,7 @@ theorem bv_axiomCheck (x y : BitVec 1) : x + y = y + x := by
   bv_decide
 
 /--
-info: 'bv_axiomCheck' depends on axioms: [propext,
- Classical.choice,
- Lean.ofReduceBool,
- Quot.sound,
- AIG.RelabelNat.State.Inv2.property]
+info: 'bv_axiomCheck' depends on axioms: [propext, Classical.choice, Lean.ofReduceBool, Quot.sound]
 -/
 #guard_msgs in
 #print axioms bv_axiomCheck
