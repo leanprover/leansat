6890bde34634163bbd0112a5db61dd3632138913
Henrik Böving <hargonix@gmail.com>
2024-07-15 11:33:13 +0200

    refactor: split out CNF

--- LeanSAT/Reflect.lean
+++ LeanSAT/Reflect.lean
@@ -3,10 +3,6 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison
 -/
-import LeanSAT.Reflect.CNF.Basic
-import LeanSAT.Reflect.CNF.ForStd
-import LeanSAT.Reflect.CNF.Relabel
-import LeanSAT.Reflect.CNF.RelabelFin
 import LeanSAT.Reflect.Tactics.Attr
 import LeanSAT.Reflect.Glue
 import LeanSAT.Reflect.Tactics.BVDecide
