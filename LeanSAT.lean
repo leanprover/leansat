27796878726c7f46fb998dcdf463526e014a149e
Henrik Böving <hargonix@gmail.com>
2024-07-15 12:10:06 +0200

    chore: top level files

--- LeanSAT.lean
+++ LeanSAT.lean
@@ -1,1 +1,13 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henrik Böving
+-/
 import LeanSAT.AIG
+import LeanSAT.BitBlast
+import LeanSAT.CNF
+import LeanSAT.External
+import LeanSAT.LRAT
+import LeanSAT.Sat
+import LeanSAT.Tactics
+import LeanSAT.Util
---
+++ LeanSAT/Tactics.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Henrik Böving
+-/
+import LeanSAT.Tactics.Attr
+import LeanSAT.Tactics.BVCheck
+import LeanSAT.Tactics.BVDecide
+import LeanSAT.Tactics.BVTrace
+import LeanSAT.Tactics.LRAT
+import LeanSAT.Tactics.Normalize
